fc1174a727fc50752ccc41950f41142c6ba9306f
Rohit Vishwakarma <53476944+Rohit77707@users.noreply.github.com>
2022-01-16 14:59:14 +0530

    Add files via upload

---
+++ Jan-11/pattern.js
@@ -0,0 +1,15 @@
+
+var ch=prompt("Enter the no");
+ch=parseInt(ch);
+
+var i,j;
+ for(i=1; i <= ch; i++)
+ {
+  for(j=1; j<=i; j++)
+ {
+   
+   document.write('*');
+  }
+   document.write('<br />');
+  }
+
